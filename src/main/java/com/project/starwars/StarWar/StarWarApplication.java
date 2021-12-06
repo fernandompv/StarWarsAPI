c1ecc09fe58c3df38cb0d1fc3e6ca1c093711e03
Fernando Piñeiro <fmpvergara@eservicios.indra.es>
2021-12-06 12:23:54 +0100

    Inicio del proyecto con los archivos de configuracion para la base de datos h2

---
+++ src/main/java/com/project/starwars/StarWar/StarWarApplication.java
@@ -0,0 +1,13 @@
+package com.project.starwars.StarWar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/starwars/StarWar/StarWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.starwars.StarWar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
