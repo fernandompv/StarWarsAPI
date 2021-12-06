3057414f3f5ecefc961feaca5eb94e8f7df59a86
Fernando Piñeiro <fmpvergara@eservicios.indra.es>
2021-12-06 13:34:23 +0100

    Añade un pequeño cambio en el parametro de configuracion del swagger para que llame a starter

--- src/main/java/com/project/starwars/StarWar/Config/SwaggerConfig.java
+++ src/main/java/com/project/starwars/StarWar/Config/SwaggerConfig.java
@@ -12,13 +12,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    private SpringSwaggerConfig springSwaggerConfig;
 
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.project.starwar.StarWar"))
+                .apis(RequestHandlerSelectors.basePackage("com.project.starter.StarWar"))
                 .paths(PathSelectors.any())
                 .build();
     }
