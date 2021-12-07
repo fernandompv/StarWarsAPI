f7ee637e8fe83b1c662b11e1dfc3e80e82c71d71
Fernando Piñeiro <fmpvergara@eservicios.indra.es>
2021-12-07 14:24:59 +0100

    Añade archivos para utilizar patrón de diseño DTO

---
+++ src/main/java/com/project/StarWar/Contexts/DTO/CastMissionDtoToMission.java
@@ -0,0 +1,4 @@
+package com.project.StarWar.Contexts.DTO;
+
+public class CastMissionDtoToMission{
+}
---
+++ src/main/java/com/project/StarWar/Contexts/DTO/MissionDTO.java
@@ -0,0 +1,44 @@
+package com.project.StarWar.Contexts.DTO;
+
+import jdk.internal.dynalink.linker.LinkerServices;
+
+import java.util.List;
+
+public class MissionDTO{
+    private String starship;
+    private List<String> Captain;
+    private List<String> planet;
+    private String startDate;
+
+    public String getStarship( ) {
+        return starship;
+    }
+
+    public void setStarship(String starship) {
+        this.starship = starship;
+    }
+
+    public List<String> getCaptain( ) {
+        return Captain;
+    }
+
+    public void setCaptain(List<String> captain) {
+        Captain = captain;
+    }
+
+    public List<String> getPlanet( ) {
+        return planet;
+    }
+
+    public void setPlanet(List<String> planet) {
+        this.planet = planet;
+    }
+
+    public String getStartDate( ) {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+}
